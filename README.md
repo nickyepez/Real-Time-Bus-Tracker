65f8638f250794a3bb3a47a970a20bb94ba2d9a5
Nick Yepez <nickyepez@NICKs-Air.attlocal.net>
2022-11-24 09:40:21 -0600

    Changed bus icons and adjusted size

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Real-Time-Bus-Tracker
 Bus tracker exercise
+
+## Test
+dsdsdsdsdsd
+sdsdsd
+### Test
+sddsdsd
+dsdsds
--- mapanimation.js
+++ mapanimation.js
@@ -1,20 +1,22 @@
 let map;
 var markers = [];
 const blueBus = {
-    url: "https://img.icons8.com/ultraviolet/2x/bus.png",
-    size: new google.maps.Size(100, 100),
-    anchor: new google.maps.Point(50, 50)
-}
+    url: "https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/512/external-bus-transport-xnimrodx-lineal-color-xnimrodx.png",
+    scaledSize: new google.maps.Size(30, 30), // scaled size
+    origin: new google.maps.Point(0, 0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor
+};
 const redBus = {
-    url: "https://img.icons8.com/fluency/2x/bus.png",
-    size: new google.maps.Size(100, 100),
-    anchor: new google.maps.Point(50, 50)
-}
+    url: "https://img.icons8.com/external-xnimrodx-lineal-color-xnimrodx/512/external-bus-coworking-space-xnimrodx-lineal-color-xnimrodx.png",
+    scaledSize: new google.maps.Size(30, 30), // scaled size
+    origin: new google.maps.Point(0, 0), // origin
+    anchor: new google.maps.Point(0, 0)
+};
 
 // load map
 function initMap() {
     var myOptions = {
-        zoom: 14,
+        zoom: 13,
         center: { lat: 42.353350, lng: -71.091525 },
         mapTypeId: google.maps.MapTypeId.TERRAIN
     };
@@ -76,7 +78,7 @@ function getIcon(bus) {
     // select icon based on bus direction
     if (bus.attributes.direction_id === 0) {
         return blueBus;
-    }  
+    }
     return redBus;
 }
 
@@ -84,7 +86,7 @@ function moveMarker(marker, bus) {
     // change icon if bus has changed direction
     var icon = getIcon(bus);
     marker.setIcon(icon);
-   
+
 
     // move icon to new lat/lon
     marker.setPosition({
